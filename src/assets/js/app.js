83426e81cb6bc9d3d8e5134481da10a8bdb653d5
XitPC <zhenyaglibko@gmail.com>
2026-08-20 21:36:32 +0300

    start faq

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,27 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-  const xl = matchMedia('(max-width: 1024px)');
+  const xl = matchMedia("(max-width: 1024px)");
 
   class Menu {
     constructor(menuElement, buttonElement) {
-      this.menu = typeof menuElement === "string" ? document.querySelector(menuElement) : menuElement;
-      this.button = typeof buttonElement === "string" ? document.querySelector(buttonElement) : buttonElement;
-      this.overlay = document.createElement('div');
+      this.menu =
+        typeof menuElement === "string"
+          ? document.querySelector(menuElement)
+          : menuElement;
+      this.button =
+        typeof buttonElement === "string"
+          ? document.querySelector(buttonElement)
+          : buttonElement;
+      this.overlay = document.createElement("div");
       this.overlay.hidden = true;
       this._init();
     }
 
     _init() {
       document.body.appendChild(this.overlay);
-      this.overlay.classList.add('overlay');
+      this.overlay.classList.add("overlay");
 
-      this.overlay.addEventListener('click', this.toggleMenu.bind(this));
-      this.button.addEventListener('click', this.toggleMenu.bind(this));
+      this.overlay.addEventListener("click", this.toggleMenu.bind(this));
+      this.button.addEventListener("click", this.toggleMenu.bind(this));
     }
 
     toggleMenu() {
-      this.menu.classList.toggle('menu--open');
-      this.button.classList.toggle('menu-button--active');
+      this.menu.classList.toggle("menu--open");
+      this.button.classList.toggle("menu-button--active");
       this.overlay.hidden = !this.overlay.hidden;
 
       if (this.isMenuOpen()) {
@@ -32,41 +37,43 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     closeMenu() {
-      this.menu.classList.remove('header__nav--active');
-      this.button.classList.remove('header__menu-button--active');
+      this.menu.classList.remove("header__nav--active");
+      this.button.classList.remove("header__menu-button--active");
       this.overlay.hidden = true;
 
       this.enableScroll();
     }
 
     isMenuOpen() {
-      return this.menu.classList.contains('menu--open');
+      return this.menu.classList.contains("menu--open");
     }
 
     disableScroll() {
-        // Get the current page scroll position;
-        const scrollTop = window.pageYOffset  || document.documentElement.scrollTop;
-        const scrollLeft = window.pageXOffset  || document.documentElement.scrollLeft;
-      
-            // if any scroll is attempted, set this to the previous value;
-            window.onscroll = function() {
-                window.scrollTo(scrollLeft, scrollTop);
-            };
+      // Get the current page scroll position;
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const scrollLeft =
+        window.pageXOffset || document.documentElement.scrollLeft;
+
+      // if any scroll is attempted, set this to the previous value;
+      window.onscroll = function () {
+        window.scrollTo(scrollLeft, scrollTop);
+      };
     }
 
     enableScroll() {
-      window.onscroll = function() {};
+      window.onscroll = function () {};
     }
   }
 
-  const menu = document.querySelector('.menu');
-  const menuButton = document.querySelector('.menu-button');
+  const menu = document.querySelector(".menu");
+  const menuButton = document.querySelector(".menu-button");
 
   if (menu && menuButton) {
     new Menu(menu, menuButton);
   }
 
-  const header = document.querySelector('header');
+  const header = document.querySelector("header");
 
   let handler;
 
@@ -75,28 +82,28 @@ document.addEventListener("DOMContentLoaded", () => {
     handler = throttle(function (event) {
       scrollHeader();
     }, 500);
-    document.addEventListener('scroll', handler, false);
+    document.addEventListener("scroll", handler, false);
   }
 
   function scrollRemove() {
     /* ... */
-    document.removeEventListener('scroll', handler, false);
+    document.removeEventListener("scroll", handler, false);
   }
 
   if (xl.matches) {
     scrollAdd();
-    document.removeEventListener('scroll', scrollHeader);
+    document.removeEventListener("scroll", scrollHeader);
   } else {
-    document.addEventListener('scroll', scrollHeader);
+    document.addEventListener("scroll", scrollHeader);
     scrollRemove();
   }
 
-  xl.addEventListener('change', () => {
+  xl.addEventListener("change", () => {
     if (xl.matches) {
-      document.removeEventListener('scroll', scrollHeader);
+      document.removeEventListener("scroll", scrollHeader);
       scrollAdd();
     } else {
-      document.addEventListener('scroll', scrollHeader);
+      document.addEventListener("scroll", scrollHeader);
       scrollRemove();
     }
   });
@@ -104,8 +111,9 @@ document.addEventListener("DOMContentLoaded", () => {
   function disableScroll() {
     // Get the current page scroll position;
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-    document.documentElement.style.setProperty('scroll-behavior', 'auto');
+    const scrollLeft =
+      window.pageXOffset || document.documentElement.scrollLeft;
+    document.documentElement.style.setProperty("scroll-behavior", "auto");
 
     // if any scroll is attempted, set this to the previous value;
     window.onscroll = function () {
@@ -114,42 +122,51 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function enableScroll() {
-    document.documentElement.style.setProperty('scroll-behavior', null);
-    window.onscroll = function () { };
+    document.documentElement.style.setProperty("scroll-behavior", null);
+    window.onscroll = function () {};
   }
 
-  var prevScrollpos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  var prevScrollpos =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop;
   function scrollHeader() {
-    var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    var currentScrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
     if (currentScrollPos < 0) {
       currentScrollPos = 0;
       prevScrollpos = 0;
-    };
+    }
     if (prevScrollpos < 0) {
       prevScrollpos = 0;
       currentScrollPos = 0;
-    };
+    }
     const num = xl.matches ? 50 : 100;
     if (currentScrollPos > num) {
-      header.classList.add('header--active');
+      header.classList.add("header--active");
     } else {
-      header.classList.remove('header--active');
-    };
+      header.classList.remove("header--active");
+    }
     if (prevScrollpos >= currentScrollPos) {
-      header.classList.remove('out');
+      header.classList.remove("out");
     } else {
-      header.classList.add('out');
-    };
+      header.classList.add("out");
+    }
     prevScrollpos = currentScrollPos;
-  };
+  }
 
   function initHeader() {
-    var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+    var currentScrollPos =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop;
     const num = xl.matches ? 50 : 150;
     if (currentScrollPos > num) {
-      header.classList.add('header--active');
+      header.classList.add("header--active");
     } else {
-      header.classList.remove('header--active');
+      header.classList.remove("header--active");
     }
   }
 
@@ -161,8 +178,8 @@ document.addEventListener("DOMContentLoaded", () => {
       savedThis;
 
     function wrapper() {
-
-      if (isThrottled) { // (2);
+      if (isThrottled) {
+        // (2);
         savedArgs = arguments;
         savedThis = this;
         return;
@@ -183,15 +200,101 @@ document.addEventListener("DOMContentLoaded", () => {
 
     return wrapper;
   }
+
+  function addMask() {
+    [].forEach.call(
+      document.querySelectorAll('input[type="tel"]'),
+      function (input) {
+        let keyCode;
+        function mask(event) {
+          event.keyCode && (keyCode = event.keyCode);
+          let pos = this.selectionStart;
+          if (pos < 3) event.preventDefault();
+          let matrix = "+7 (___) ___-__-__",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function (a) {
+              return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+            });
+          i = new_value.indexOf("_");
+          if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i);
+          }
+          let reg = matrix
+            .substr(0, this.value.length)
+            .replace(/_+/g, function (a) {
+              return "\\d{1," + a.length + "}";
+            })
+            .replace(/[+()]/g, "\\$&");
+          reg = new RegExp("^" + reg + "$");
+          if (
+            !reg.test(this.value) ||
+            this.value.length < 5 ||
+            (keyCode > 47 && keyCode < 58)
+          )
+            this.value = new_value;
+          if (event.type == "blur" && this.value.length < 5) {
+            this.value = "";
+            this.classList.remove("havetext");
+          }
+        }
+
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false);
+        input.value
+          ? input.classList.add("havetext")
+          : input.classList.remove("havetext");
+      },
+    );
+  }
+  addMask();
+
+  class ReviewsShowMore extends window.HTMLElement {
+    connectedCallback() {
+      this.init();
+    }
+    init() {
+      if (this.classList.contains("inited")) {
+        return;
+      }
+
+      this.addEventListener("click", function () {
+        this.classList.toggle("active");
+      });
+    }
+  }
+  if (!customElements.get("reviews-showmore")) {
+    window.customElements.define("reviews-showmore", ReviewsShowMore);
+  }
+
+  const reviews = document.querySelectorAll(".reviews-swiper");
+  if (reviews.length) {
+    reviews.forEach((swiper) => {
+      const slides = swiper.querySelectorAll(".swiper-slide");
+      const pagination = swiper.querySelector(".swiper-pagination");
+      
+      new Swiper(swiper, {
+        slidesPerView: "auto",
+        grabCursor: true,
+        loop: slides?.length > 3,
+        pagination: {
+          el: pagination,
+        },
+        on: {
+          beforeTransitionStart: () => {
+            const showmore = swiper.querySelectorAll(".reviews-showmore");
+            if (showmore.length) {
+              showmore.forEach((el) => {
+                el.classList.remove("active");
+              });
+            }
+          },
+        },
+      });
+    });
+  }
 });
-
-
-
-
-
-
-
-
-
-
-
